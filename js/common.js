4007cd73d89a663f00848c627c60853e0c6a7354
joosun0823 <133857203+joosun0823@users.noreply.github.com>
2023-07-21 10:02:33 +0900

    434

--- js/common.js
+++ js/common.js
@@ -1,21 +1,67 @@
-const elHeader = document.querySelector('header .head');
+$('body').prepend('<header></header>');
+$('header').load('./header.html', afterLoad);
 
 
-function init() {
-
-function scrollFun() {
-    let startSCroll = elHeader.getBoundingClientRect().y;
-    if(window.pageYOffset > 0) {
-        elHeader.classList.remove("active");
-    } else {
-        elHeader.classList.add("active");
-    };
-
+function afterLoad() {
+    const burger = document.querySelector(".hamburg > a");
+    const main = document.querySelector("main");
+    const header = document.querySelector("header")
     
+    
+    function headerTop() {
+        $(window).scroll(function() {
+            if( window.pageYOffset <= 0) {
+                $(".head").addClass("fixed")
+            } else {
+                $(".head").removeClass("fixed")
+            }
+        })
+    }
+    headerTop();
+
+    function headerSlide() {
+        $(".master-innerBox > li > a").on("mouseenter",function(){
+            $(".master-innerBox").addClass("active")
+            $("header").addClass("active")
+        })
+        $("header").on("mouseleave",function(){
+            $(".master-innerBox").removeClass("active")
+            $("header").removeClass("active")
+        })
+    }
+    headerSlide();
+
+    burger.addEventListener("click", function() {
+        document.body.classList.toggle("ds-none");
+        
+        header.classList.toggle("open");
+        
+    })
+
 }
 
+const ArrowBttn = document.querySelector('.arrow');
+const footer = document.querySelector("footer");
+const header = document.querySelector(".hehe");
 
-    window.addEventListener("scroll", scrollFun)
-}
+ArrowBttn.addEventListener("click", function() {
+    console.log(ArrowBttn);
+    console.log(ArrowBttn.classList.contains('active'));
+    if(ArrowBttn.classList.contains('active')) {
+            footer.scrollIntoView({behavior: 'smooth' });
+        } else {
+            header.scrollIntoView({behavior: 'smooth' });
+        }
+        });
 
-window.addEventListener("load", init);
+const obSide = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(function(entriesV,entriesK) {
+        if(entriesV.isIntersecting){
+            ArrowBttn.classList.add('active')
+        } else {
+            ArrowBttn.classList.remove('active')
+        }
+    })
+});
+
+obSide.observe(header);
---
+++ js/index.js
@@ -0,0 +1,22 @@
+const firstWrap = document.querySelector(".sec-1");
+const secondWrap = document.querySelector(".sec-2");
+const thirdWrap = document.querySelector(".sec-3");
+const fourthWrap = document.querySelector(".sec-4");
+
+const ob = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(function(entriesV, entriesK) {
+        if(entriesV.isIntersecting) {
+            entriesV.target.classList.add("active")
+            console.log(entriesV);
+        } else {
+            entriesV.target.classList.remove("active")
+        }
+    })
+});
+
+
+
+ob.observe(firstWrap);
+ob.observe(secondWrap);
+ob.observe(thirdWrap);
+ob.observe(fourthWrap);
